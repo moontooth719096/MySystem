ed9a85b3e6265694706fa7fb6ff5b0b04b7d52f9
GuoWeiWu <moontooth719096@gmail.com>
2017-08-29 22:58:19 +0800

    新增功能頁面

---
+++ MySystem/Controllers/Fumction/FunctionController.cs
@@ -0,0 +1,23 @@
+﻿using MySystem.Models.Function;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MySystem.Controllers.Fumction
+{
+    public class FunctionController : Controller
+    {
+        //
+        // GET: /Function/
+
+        public ActionResult MainFunctionPage()
+        {
+            MainFunction model = new MainFunction();
+
+            return View(model);
+        }
+
+    }
+}
