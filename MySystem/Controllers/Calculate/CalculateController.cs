c7c6ce73e111c534348d572ddaa8f09ca7e78208
GuoWeiWu <moontooth719096@gmail.com>
2018-02-11 11:18:31 +0800

    新增抓樂罷TP計算

---
+++ MySystem/Controllers/Calculate/CalculateController.cs
@@ -0,0 +1,19 @@
+﻿using MySystem.ViewModels.Function;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MySystem.Controllers
+{
+    public class CalculateController : Controller
+    {
+        public ActionResult TorebaTPConvert()
+        {
+            string message = string.Empty;
+            return View();
+        }
+
+    }
+}
